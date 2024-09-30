28efa4b20cacf125ab5b7fe3cd74b0d59e0defbe
C <142123173+cjbuisness@users.noreply.github.com>
2024-09-30 09:45:13 -0700

    Update server.js
    app

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { createObjectCsvWriter } = require('csv-writer');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Middleware
 app.use(bodyParser.json());
